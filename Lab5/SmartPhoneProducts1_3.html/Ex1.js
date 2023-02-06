b0999858d6d56b90675becfff268381b6ad67876
anmell <alanna.mellor@gmail.com>
2023-02-05 23:42:44 -1000

    WOD and Lab4

---
+++ Lab5/SmartPhoneProducts1_3.html/Ex1.js
@@ -0,0 +1,10 @@
+age =  20;
+fav_num = 22;
+day_of_birth = 30;
+month_of_birth = 3;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+age =  20;
+fav_num = 22;
+day_of_birth = 30;
+month_of_birth = 3;
