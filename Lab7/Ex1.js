accea82e4bbc899b321e1847d9d4294685db15e7
anmell <alanna.mellor@gmail.com>
2023-02-16 13:17:33 -1000

    ClassWork

--- Lab7/Ex1.js
+++ Lab7/Ex1.js
@@ -1,7 +1,18 @@
 require("./products_data.js"); 
 var num_products = 5;
-var number =0;
-while(number++ < (num_products)){
+var prod_number =0;
+while(prod_number < num_products){
+   prod_number++;
+   if (prod_number>.25*num_products && prod_number<.75*num_products) {
+      console.log(`${eval("name" + prod_number)} is sold out!`)
+      continue;
+   }
    
-   console.log(number)  
+   console.log(`${prod_number}. ${eval('name' + prod_number)}`);  
+   
+   if(prod_number > num_products/2) {
+      console.log(`Don't ask for anything else!`)
+      process.exit();
+   }; 
 };
+console.log ("That's all we have!");
