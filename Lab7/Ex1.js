5011fdd2d0ab9313e433cb1d88f794b667435cf5
anmell <alanna.mellor@gmail.com>
2023-02-21 13:18:30 -1000

    lab8

--- Lab7/Ex1.js
+++ Lab7/Ex1.js
@@ -1,18 +1,21 @@
 require("./products_data.js"); 
 var num_products = 5;
 var prod_number =0;
-while(prod_number < num_products){
+for (;prod_number < num_products;){
+   
    prod_number++;
+
+if (eval("typeof name"+prod_number) != 'undefined') {
    if (prod_number>.25*num_products && prod_number<.75*num_products) {
       console.log(`${eval("name" + prod_number)} is sold out!`)
       continue;
    }
-   
+}
    console.log(`${prod_number}. ${eval('name' + prod_number)}`);  
-   
+
    if(prod_number > num_products/2) {
       console.log(`Don't ask for anything else!`)
       process.exit();
    }; 
 };
-console.log ("That's all we have!");m
+console.log ("That's all we have!");
