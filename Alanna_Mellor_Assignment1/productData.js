aa533cfa97da6b5bf47cfa433313706fa0c63f5a
anmell <alanna.mellor@gmail.com>
2023-03-01 17:40:30 -1000

    werk

--- Alanna_Mellor_Assignment1/productData.js
+++ Alanna_Mellor_Assignment1/productData.js
@@ -1,4 +1,4 @@
-[
+var products = [
     {"name":"Queen of Hearts", 
     "img": "https://media.istockphoto.com/id/165931595/vector/queen-of-hearts-playing-card.jpg?s=612x612&w=is&k=20&c=S_hMf9dyALl1jxcNBhGerTJTuQcp9_UZHRCWne3eCyc=", 
     "description": "Limited Edition 1990 Queen of Hearts Playing Card", "price": "100.00", 
@@ -24,4 +24,4 @@
     "img": "https://media.istockphoto.com/id/165931595/vector/queen-of-hearts-playing-card.jpg?s=612x612&w=is&k=20&c=S_hMf9dyALl1jxcNBhGerTJTuQcp9_UZHRCWne3eCyc=", 
     "description": "Limited Edition 1990 Queen of Hearts Playing Card", "price": "100.00", 
     "qa": "10"}
-]
+];
