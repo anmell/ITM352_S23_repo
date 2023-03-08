280cc1bd8178fc96e7f625204fd843f2556287de
anmell <alanna.mellor@gmail.com>
2023-03-08 13:04:37 -1000

    wod

--- WODs/Invoice3/products_data.js
+++ WODs/Invoice3/products_data.js
@@ -1,25 +1,3 @@
-console.log("Product variables loaded...");
-    /* var name1 = "HTC";
-     var price1 = 40.00;
-     var image1 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg";
-
-     var name2 = "Apple";
-     var price2 = 75.00;
-     var image2 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg";
-
-     var name3 = "Nokia";
-     var price3 = 35.00;
-     var image3 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg";
-
-     var name4 = "Samsung";
-     var price4 = 45.00;
-     var image4 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg";
-
-     var name5 = "Blackberry";
-     var price5 = 10.00;
-     var image5 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg"; 
-     */
-
      var products = [
           {'brand': 'HTC', 'price': '40.00', 'image': 'http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg'},
           {'brand': 'Apple', 'price': '75.00', 'image': 'http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg'},
@@ -28,6 +6,7 @@ console.log("Product variables loaded...");
           {'brand': 'Blackberry', 'price': '10.00', 'image': 'http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg'}
      ];
 
-     var quantities = [
-          2, 1, 1, 3, 12
-     ];
+     var quantities = 
+     [
+          2,1,1,-3,12
+     ]
