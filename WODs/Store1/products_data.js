736f809fc4cb5825c2de256e78ecdc4a60df8cbc
anmell <alanna.mellor@gmail.com>
2023-03-26 23:08:37 -1000

    wod

---
+++ WODs/Store1/products_data.js
@@ -0,0 +1,20 @@
+console.log("Product variables loaded...");
+     var products = [
+          {'brand': 'HTC', 'price': '40.00', 'image': 'http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg'},
+          {'brand': 'Apple', 'price': '75.00', 'image': 'http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg'},
+          {'brand': 'Nokia', 'price': '35.00', 'image': 'http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg'},
+          {'brand': 'Samsung', 'price': '45.00', 'image': 'http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg'},
+          {'brand': 'Blackberry', 'price': '10.00', 'image': 'http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg'}
+     ];
+
+     var quantities = [
+          {'name': 'quantity0', 'id': 'quantity0_label'},
+          {'name': 'quantity1', 'id': 'quantity1_label'},
+          {'name': 'quantity2', 'id': 'quantity2_label'},
+          {'name': 'quantity3', 'id': 'quantity3_label'},
+          {'name': 'quantity4', 'id': 'quantity4_label'}
+     ];
+
+     var quantity_ids = [
+          "quantity1_label", "quantity2_label", "quantity3_label", "quantity4_label"
+     ];
