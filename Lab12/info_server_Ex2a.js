0681fd5330338d263fa9dae2fdc3502555fd73c3
anmell <alanna.mellor@gmail.com>
2023-03-30 13:14:10 -1000

    lab12

--- Lab12/info_server_Ex2a.js
+++ Lab12/info_server_Ex2a.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 app.all('*', function (request, response, next) {
-    response.send(request.method + ' to path ' + request.path);
+    console.log(request.method + ' to path ' + request.path + ` with qs ` + JSON.stringify(request.query));
 });
-app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
+app.use(express.static(__dirname + '/public'));
+app.listen(8081, () => console.log(`listening on port 8081`)); // note the use of an anonymous function here to do a callback
