cbb2e71461c40d8aa4251c2a53b2f43c05d2e152
anmell <alanna.mellor@gmail.com>
2023-04-04 23:47:38 -1000

    assignment1

---
+++ Lab12/server.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var app = express();
+var querystring = require('querystring');
+
+var products = require(__dirname + '/product_data.json'); // in current directory, get products.json and store it in memory in the server; loading json and displaying it in products
+
+
+// Routing 
+
+// allows server to share products data to any user that asks for it and allows server to control product data; server is database
+app.get("/product_data.js", function (request, response, next) { // respond with js by giving var products = product data in memory and turns it into a json string and sends it back 
+   response.type('.js'); // send response as javascript
+   var products_str = `var products = ${JSON.stringify(products)};`; // string is executed and defines var products 
+   response.send(products_str); // sends back string with all the javascript; respond.send has to send a string
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+// monitor all requests
+app.all('*', function (request, response, next) {
+   console.log(request.method + ' to ' + request.path);
+   next();
+});
+
+// process purchase request (validate quantities, check quantity available)
+
+// ** Insert Code Here **
+
+// Get quantities from products_display.html and create invoice if valid
+app.post('/process_purchase', function (request, response, next) {
+   console.log(request.body);
+
+   // check if quantities are valid, if yes, send to invoice with query string of data
+   
+   // quantities are valid, send to invoice
+   for(let i in products) {
+      console.log(request.body[`quantity${i}`], FindStringisNonNegInt(request.body[`quantity${i}`]));
+   }
+
+   // quantities are valid, remove quantities purchased from quantities_available
+   for(let i in products) {
+      products[i].quantity_available -= request.body[`quantity${i}`];
+   }
+   response.redirect('./invoice.html?' + querystring.stringify(request.body));
+     
+
+});
+
+// route all other GET requests to files in public 
+app.use(express.static(__dirname + '/public'));
+
+// start server
+app.listen(8080, () => console.log(`listening on port 8080`));
+
+// Loads products data from json file into server memory
+var products_array = require(__dirname + '/product_data.json');
+
+
+function FindStringisNonNegInt(q, returnErrors=false) {
+   // Checks if a string q is a non negative integer; if returnErrors is true, the array of errors is returned
+   // On the contrary, it will return true if q is a non negative integer
+   errors = []; // assume no errors at first
+   if(Number(q) != q) errors.push('Please enter a number!'); // Check if string is a number value
+   if(q < 0) errors.push('Please enter a non-negative value!'); // Check if it is non-negative
+   if(parseInt(q) != q) errors.push('This is not an integer!'); // Check that it is an integer
+
+   return (returnErrors ? errors : (errors.length == 0));
+};
--- Mellor_Alanna_Assignment1/server.js
+++ Mellor_Alanna_Assignment1/server.js
@@ -2,68 +2,52 @@ var express = require('express');
 var app = express();
 var querystring = require('querystring');
 
-var products = require(__dirname + '/product_data.json'); // in current directory, get products.json and store it in memory in the server; loading json and displaying it in products
-
-
 // Routing 
 
-// allows server to share products data to any user that asks for it and allows server to control product data; server is database
-app.get("/product_data.js", function (request, response, next) { // respond with js by giving var products = product data in memory and turns it into a json string and sends it back 
-   response.type('.js'); // send response as javascript
-   var products_str = `var products = ${JSON.stringify(products)};`; // string is executed and defines var products 
-   response.send(products_str); // sends back string with all the javascript; respond.send has to send a string
-});
-
-app.use(express.urlencoded({ extended: true }));
-
-// monitor all requests
+// monitor all requests; this manages what is output in the console for all requests
 app.all('*', function (request, response, next) {
    console.log(request.method + ' to ' + request.path);
    next();
 });
 
-// process purchase request (validate quantities, check quantity available)
 
-// ** Insert Code Here **
+// stores product json data in server memory undet the variable name 'products' and maes it accessible through the root directory and the following json file path
+var products = require(__dirname + '/product_data.json');
 
-// Get quantities from products_display.html and create invoice if valid
-app.post('/process_purchase', function (request, response, next) {
-   console.log(request.body);
 
-   // check if quantities are valid, if yes, send to invoice with query string of data
-   
-   // quantities are valid, send to invoice
-   for(let i in products) {
-      console.log(request.body[`quantity${i}`], FindStringisNonNegInt(request.body[`quantity${i}`]));
-   }
-
-   // quantities are valid, remove quantities purchased from quantities_available
-   for(let i in products) {
-      products[i].quantity_available -= request.body[`quantity${i}`];
-   }
-
-   response.redirect('./invoice.html?' + querystring.stringify(request.body));
-     
-
+// when the server recieves a GET request for "/product_data.js", the server will respong in javascript with a string of data provided by the JSON file
+app.get("/product_data.js", function (request, response, next) {
+   response.type('.js');
+   var products_str = `var products = ${JSON.stringify(products)};`;
+   response.send(products_str);
 });
 
-// route all other GET requests to files in public 
-app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
 
-// start server
-app.listen(8080, () => console.log(`listening on port 8080`));
+// process purchase request (validate quantities, check quantity available)
 
-// Loads products data from json file into server memory
-var products_array = require(__dirname + '/product_data.json');
+// load product data into server; make it accessible via a GET request; when a client send a GET request
 
 
-function FindStringisNonNegInt(q, returnErrors=false) {
-   // Checks if a string q is a non negative integer; if returnErrors is true, the array of errors is returned
-   // On the contrary, it will return true if q is a non negative integer
-   errors = []; // assume no errors at first
-   if(Number(q) != q) errors.push('Please enter a number!'); // Check if string is a number value
-   if(q < 0) errors.push('Please enter a non-negative value!'); // Check if it is non-negative
-   if(parseInt(q) != q) errors.push('This is not an integer!'); // Check that it is an integer
+// <** your code here ***>
 
-   return (returnErrors ? errors : (errors.length == 0));
-};
+// route to get to invoice page
+app.post('/invoice.html', function (request, response) {
+for (let i in products) {
+request.body [`quantities${i}`];
+response.sendFile(__dirname + '/public/invoice.html')
+}});
+// the following allows access to the body when the server recieves a POST request 
+ 
+
+//check if quantities are valid via the NonNegInt function; call the function
+// if valid, send to invoice page;
+
+
+
+/* enable server to respond to requests for static files (files that are not intended to have any server-processing); files must be located in a directory called "public"; the following uses the Express static middleware component */
+app.use(express.static(__dirname + '/public')); 
+
+// starts the server
+app.listen(8080, () => console.log(`listening on port 8080`))
+
