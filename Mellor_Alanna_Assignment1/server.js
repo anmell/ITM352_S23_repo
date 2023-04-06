5120a40097ade7632ebb5fdddaaf9e82247074f6
anmell <alanna.mellor@gmail.com>
2023-04-05 17:32:38 -1000

    assignment1

--- Mellor_Alanna_Assignment1/server.js
+++ Mellor_Alanna_Assignment1/server.js
@@ -2,6 +2,16 @@ var express = require('express');
 var app = express();
 var querystring = require('querystring');
 
+function isNonNegInt(quantities, returnErrors) {
+   errors = []; // assume no errors at first
+   if (Number(quantities) != quantities) errors.push('Not a number!'); // Check if string is a number value
+   if (quantities < 0) errors.push('Negative value!'); // Check if it is non-negative
+   if (parseInt(quantities) != quantities) errors.push('Not an integer!'); // Check that it is an integer
+
+   var returnErrors = returnErrors ? errors : (errors.length == 0);
+   return (returnErrors);
+ };
+
 // Routing 
 
 // monitor all requests; this manages what is output in the console for all requests
@@ -32,10 +42,23 @@ app.use(express.urlencoded({ extended: true }));
 // route to get to invoice page
 app.post('/invoice.html', function (request, response) {
    console.log(request.body);
- 
+for (let i in products) {
+   var qty = request.body [`quantities${i}`];
+   let errors = isNonNegInt(qty, true);
+   if (errors.length > 0) {
+      response.type('.js');
+      response.send(`Hi please fix the following errors: ${errors.join()}`);
+      console.log(errors)
+   }
+   if (qty == 0 ){
+      response.send (`please enter a quantity in any textbox`)
+   }else{
+      response.redirect('./invoice.html?' + querystring.stringify(request.body));
+    }}});
+// if there are invalid quantities are entered, send an alert to user
+// else send user to invoice page 
+
    // Pass the quantities data to the invoice.html template
-   response.redirect('./invoice.html?' + querystring.stringify(request.body));
- });
 
 
 
