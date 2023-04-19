f8d598f7eb2f7eb3fdd9d009549d7932184899fa
anmell <alanna.mellor@gmail.com>
2023-04-19 12:37:27 -1000

    assignment2

--- Mellor_Alanna_Assignment2/server.js
+++ Mellor_Alanna_Assignment2/server.js
@@ -10,6 +10,11 @@ var querystring = require('querystring');
 // stores product json data in server memory undet the variable name 'products' and maes it accessible through the root directory and the following json file path
 var products = require(__dirname + '/product_data.json');
 
+//variable to collect temporary data (like quantities and user info)
+var transient_data = {};
+
+var params = new URLSearchParams(transient_data);
+
 
 // function to check is quantities entered are (1) whole numbers, (2) not negative, and (3) a number and not a word or character; taken from previous labs
 function isNonNegInt(quantities, returnErrors) {
@@ -96,7 +101,6 @@ app.post('/login.html', function (request, response) {
       // if the user selects more quantities than are available, push an error into the errors_array
       if (qty > qa) {
          errors_array.push(`The quantity that you have selected for ${name} exceeds the quantity that we have available`);
-
       }
    }
 
@@ -106,26 +110,32 @@ app.post('/login.html', function (request, response) {
    // if every validation passes, update the total sold property when you update the quantities available property
 
 
-   // if quantities entered passes every validation, run a loop through all products to update quantity_selected for each 
+   /*// if quantities entered passes every validation, run a loop through all products to update quantity_selected for each 
    for (let i in products) {
 
       if (errors_array.length == 0) {
          // push the values that the user has entered to quantity_selected for each product 
          products[i].quantity_selected += Number(request.body[`quantities${i}`]);
       }
-   }
+   }*/
 
 
-   // after evaluating all the data and updating quantitiesAvailable and total_sold, decide if the client should be sent back to products_display (meaning at least one validation was not passed) or move forward to the invoice page (all data entered is valid)
-   for (let i = 0; i < products.length; i++) {
+   // after evaluating all the data and updating quantitiesAvailable and total_sold, decide if the client should be sent back to products_display (meaning at least one validation was not passed) or move forward to the login page (all data entered is valid)
 
       // if errors_array.length is 0, then they can go to the invoice page. If array is not empty, send them back to the products_display page
       if (errors_array.length == 0) {
-         response.redirect('./login.html?'/*+ querystring.stringify ({selected_quantities:`${JSON.stringify(selected_quantities)}`})*/);
+         
+         for (let i in products) {
+            transient_data = request.body;
+            console.log(transient_data)
+            products[i].total_sold += request.body[`quantities${i}`];
+            products[i].quantityAvailable -= request.body[`quantities${i}`];
+         }
+         response.redirect('./login.html?' + querystring.stringify({transient_data: `${JSON.stringify(transient_data)}` }));
+
       } else {
          response.redirect('./product_display.html?' + querystring.stringify({ ...request.body, errors_array: `${JSON.stringify(errors_array)}` }));
       }
-   }
 });
 
 
@@ -133,6 +143,7 @@ app.post('/login.html', function (request, response) {
 
 // load file system (fs) interface
 var fs = require('fs');
+const { request } = require('http');
 
 var filename = __dirname + '/user_data.json';
 
@@ -158,32 +169,69 @@ app.post("/invoice.html", function (request, response) {
 
    //if username does not exist, redirect back to login.html, pass error via query string
    if (!user_data.hasOwnProperty(`${username}`)) {
-      error_check.push(`The username you've entered does not exist, please create a new account`)
-      response.redirect('./login.html?' + querystring.stringify({ ...request.body, error_check: `${JSON.stringify(error_check)}` }));
+      error_check.push(`The email you've entered does not exist, please create a new account`)
+      response.redirect('./login.html?' + querystring.stringify({error_check: `${JSON.stringify(error_check)}` }) + params.toString());
    }
 
    // if username does exist, but password does match, redict back to login.html, pass error via query string
    if (user_data.hasOwnProperty(`${username}`) && password !== user_data[username][`password`]) {
       error_check.push(`Incorrect password for ${username}`)
-      response.redirect('./login.html?' + querystring.stringify({ ...request.body, error_check: `${JSON.stringify(error_check)}` }));
+      response.redirect('./login.html?' + querystring.stringify({ ...request.body, error_check: `${JSON.stringify(error_check)}` }) + params.toString());
    }
 
 
    //check if the username exists in the user_data file and that the password matches appropriately
    if (user_data.hasOwnProperty(`${username}`) && password == user_data[username][`password`]) {
-      for (let i in products) {
-         products[i].total_sold += Number(products[i].quantity_selected);
-         products[i].quantityAvailable -= Number(products[i].quantity_selected);
-      }
       //if validation passes, direct to invoice page; second half of this equation is from Assignment2 Code Examples 
-      response.redirect('./invoice.html?');
+      response.redirect('./invoice.html?'+ querystring.stringify({username: `${JSON.stringify(username)}` }) + params.toString());
 
    }
+});
+
+app.get('/product_display.html', function (request, response) {
+ // Reset quantity_selected for all products back to zero when product_display is requested (GET) after an invoice has already been generated 
+for (let i in products) {
+products[i].quantity_selected = 0;  
+}
+
+// can't use response.redirect here because the server gets into an endless loop of redirecting to this file nonstop (since response.redirect is always a GET request)
+response.sendFile (__dirname + '/public' + '/product_display.html')
 });
 
 
 // Registration
 
+var reg_errors = []; // keep errors on server to share with registration page
+var successful_login = []
+
+// the following is inspired by assignment 2 code examples
+app.post('/registration.html', function (request, response) {
+   // process a simple register form
+   username = request.body.email.toLowerCase();
+
+   // check that all required fields have been filled out
+if(request.body.email == '' || request.body.name == ''|| request.body.password == '' || request.body.repeat_password == '') {
+       reg_errors.push('Please fill out all fields');
+   }
+   // check if is username taken
+   if(typeof user_data[username] != 'undefined') {
+       reg_errors.push(`Sorry! ${username} is already taken. Please select a new username`);
+   }
+   if(request.body.password != request.body.repeat_password) {
+       reg_errors.push('The passwords entered do not match');
+   } 
+   
+   if(Object.keys(reg_errors).length == 0) {
+      user_data[username] = {};
+      user_data[username].password = request.body.password;
+      user_data[username].email = request.body.email;
+       fs.writeFileSync(filename, JSON.stringify(user_data));
+      successful_login.push(`Your account has been registered!`)
+       response.redirect('./login.html?' + querystring.stringify({successful_login: `${JSON.stringify(successful_login)}` })+ params.toString());
+   } else {
+       response.redirect("./registration.html?" + querystring.stringify({ ...request.body, reg_errors: `${JSON.stringify(reg_errors)}` })+params.toString());
+   }
+});
 
 /* enable server to respond to requests for static files (files that are not intended to have any server-processing); files must be located in a directory called "public"; the following uses the Express static middleware component */
 app.use(express.static(__dirname + '/public'));
